a0c98530138b14391058c6d997ca25fde0bf47f0
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2024-03-10 21:53:20 +0100

    fix: istio demo kind cluster
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- demo/istio/bootstrap.sh
+++ demo/istio/bootstrap.sh
@@ -3,12 +3,31 @@
 KIND_IMAGE=kindest/node:v1.29.2
 ISTIO_REPO=https://istio-release.storage.googleapis.com/charts
 ISTIO_NS=istio-system
+ISTIO_INGRESS_NS=istio-ingress
 
 # Create Kind cluster
-kind create cluster --image $KIND_IMAGE
+kind create cluster --image $KIND_IMAGE --wait 1m --config - <<EOF
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+  - role: control-plane
+    kubeadmConfigPatches:
+      - |-
+        kind: InitConfiguration
+        nodeRegistration:
+          kubeletExtraArgs:
+            node-labels: "ingress-ready=true"
+    extraPortMappings:
+      - containerPort: 80
+        hostPort: 80
+        protocol: TCP
+      - containerPort: 443
+        hostPort: 443
+        protocol: TCP
+  - role: worker
+EOF
 
 # Install Istio components
-helm upgrade --install istio-base       --namespace $ISTIO_NS --create-namespace --wait --repo $ISTIO_REPO base
-helm upgrade --install istiod           --namespace $ISTIO_NS --create-namespace --wait --repo $ISTIO_REPO istiod
-helm upgrade --install istio-ingress    --namespace $ISTIO_NS --create-namespace --wait --repo $ISTIO_REPO gateway
-
+helm upgrade --install istio-base       --namespace $ISTIO_NS           --create-namespace --wait --repo $ISTIO_REPO base
+helm upgrade --install istiod           --namespace $ISTIO_NS           --create-namespace --wait --repo $ISTIO_REPO istiod
+helm upgrade --install istio-ingress    --namespace $ISTIO_INGRESS_NS   --create-namespace --wait --repo $ISTIO_REPO gateway

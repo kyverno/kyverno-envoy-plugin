f7f0f119f39a164c759e478d38c98b9aefcb2339
Sanskarzz <sanskar.gur@gmail.com>
2024-03-13 22:35:16 +0530

    pref: added gracefulshutdown and system events
    Signed-off-by: Sanskarzz <sanskar.gur@gmail.com>

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"google.golang.org/grpc"
 )
@@ -18,14 +23,25 @@ func NewServers() *Servers {
 	return &Servers{}
 }
 
-func (s *Servers) startHTTPServer() {
+func (s *Servers) startHTTPServer(ctx context.Context) {
 
 	s.httpServer = &http.Server{
 		Addr:    ":8080",
 		Handler: http.HandlerFunc(handler),
 	}
 	fmt.Println("Starting HTTP server on Port 8080")
-	if err := s.httpServer.ListenAndServe(); err != nil {
+	go func() {
+		<-ctx.Done()
+
+		fmt.Println("HTTP server shutting down...")
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err := s.httpServer.Shutdown(ctx); err != nil && err != http.ErrServerClosed {
+			log.Fatal("Shutdown HTTP server:", err)
+		}
+	}()
+
+	if err := s.httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
@@ -34,7 +50,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello World!")
 }
 
-func (s *Servers) startGRPCServer() {
+func (s *Servers) startGRPCServer(ctx context.Context) {
 
 	lis, err := net.Listen("tcp", ":9090")
 	if err != nil {
@@ -42,16 +58,33 @@ func (s *Servers) startGRPCServer() {
 	}
 	s.grpcServer = grpc.NewServer()
 	fmt.Println("Starting GRPC server on Port 9090")
+
+	go func() {
+		<-ctx.Done()
+		if s.grpcServer != nil {
+			fmt.Println("GRPC server shutting down...")
+			s.grpcServer.GracefulStop()
+		}
+	}()
+
 	if err := s.grpcServer.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
 
 func main() {
+	httpCtx, httpCancel := context.WithCancel(context.Background())
+	grpcCtx, grpcCancel := context.WithCancel(context.Background())
 
 	srv := NewServers()
-	go srv.startHTTPServer()
-	go srv.startGRPCServer()
-	select {}
+	go srv.startHTTPServer(httpCtx)
+	go srv.startGRPCServer(grpcCtx)
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, syscall.SIGINT, syscall.SIGTERM)
+
+	<-done
+	httpCancel()
+	grpcCancel()
+	time.Sleep(5 * time.Second)
 
 }

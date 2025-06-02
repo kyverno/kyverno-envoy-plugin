8ebe07cb6f555e0d2c799d06d7eac1cbeb820d99
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2025-06-02 22:37:17 +0200

    refactor: use utils in jwt lib (#372)
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/authz/cel/libs/jwt/impl.go
+++ pkg/authz/cel/libs/jwt/impl.go
@@ -4,6 +4,7 @@ import (
 	"github.com/golang-jwt/jwt"
 	"github.com/google/cel-go/common/types"
 	"github.com/google/cel-go/common/types/ref"
+	"github.com/kyverno/kyverno-envoy-plugin/pkg/authz/cel/utils"
 	"google.golang.org/protobuf/types/known/structpb"
 )
 
@@ -12,34 +13,32 @@ type impl struct {
 }
 
 func (c *impl) decode(token ref.Val, key ref.Val) ref.Val {
-	t, ok := token.(types.String)
-	if !ok {
-		return types.MaybeNoSuchOverloadErr(token)
-	}
-	k, ok := key.(types.String)
-	if !ok {
-		return types.MaybeNoSuchOverloadErr(key)
-	}
-	claimsMap := jwt.MapClaims{}
-	parsed, err := jwt.ParseWithClaims(string(t), claimsMap, func(*jwt.Token) (any, error) {
-		return []byte(k), nil
-	})
-	if err != nil {
-		return c.NativeToValue(nil)
-	}
-	header, err := structpb.NewStruct(parsed.Header)
-	if err != nil {
+	if token, err := utils.ConvertToNative[string](token); err != nil {
 		return types.WrapErr(err)
-	}
-	claims, err := structpb.NewStruct(claimsMap)
-	if err != nil {
+	} else if key, err := utils.ConvertToNative[string](key); err != nil {
 		return types.WrapErr(err)
+	} else {
+		claimsMap := jwt.MapClaims{}
+		parsed, err := jwt.ParseWithClaims(token, claimsMap, func(*jwt.Token) (any, error) {
+			return []byte(key), nil
+		})
+		if err != nil {
+			return c.NativeToValue(nil)
+		}
+		header, err := structpb.NewStruct(parsed.Header)
+		if err != nil {
+			return types.WrapErr(err)
+		}
+		claims, err := structpb.NewStruct(claimsMap)
+		if err != nil {
+			return types.WrapErr(err)
+		}
+		return c.NativeToValue(
+			Token{
+				Header: header,
+				Claims: claims,
+				Valid:  parsed.Valid,
+			},
+		)
 	}
-	return c.NativeToValue(
-		Token{
-			Header: header,
-			Claims: claims,
-			Valid:  parsed.Valid,
-		},
-	)
 }

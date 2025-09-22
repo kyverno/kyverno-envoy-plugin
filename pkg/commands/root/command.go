f0f6f20fbf8b101b3deb21efd578a16eb9bc4f10
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2025-09-22 18:11:22 +0200

    feat: add version command (#532)
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/commands/root/command.go
+++ pkg/commands/root/command.go
@@ -2,14 +2,22 @@ package root
 
 import (
 	"github.com/kyverno/kyverno-envoy-plugin/pkg/commands/serve"
+	"github.com/kyverno/kyverno-envoy-plugin/pkg/commands/version"
 	"github.com/spf13/cobra"
 )
 
 func Command() *cobra.Command {
 	root := &cobra.Command{
-		Use:   "kyverno-envoy-plugin",
-		Short: "kyverno-envoy-plugin is a plugin for Envoy",
+		Use:          "kyverno-envoy-plugin",
+		Short:        "kyverno-envoy-plugin is a plugin for Envoy",
+		SilenceUsage: true,
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			return cmd.Help()
+		},
 	}
-	root.AddCommand(serve.Command())
+	root.AddCommand(
+		serve.Command(),
+		version.Command(),
+	)
 	return root
 }
---
+++ pkg/version/version.go
@@ -0,0 +1,67 @@
+package version
+
+import (
+	"runtime/debug"
+)
+
+const (
+	notFound    = "---"
+	vcsTime     = "vcs.time"
+	vcsRevision = "vcs.revision"
+)
+
+// BuildVersion is provided at compile-time
+var BuildVersion string
+
+type buildInfoReader = func() (*debug.BuildInfo, bool)
+
+func Version() string {
+	return version(debug.ReadBuildInfo)
+}
+
+func version(reader buildInfoReader) string {
+	if BuildVersion == "" {
+		bi, ok := reader()
+		if !ok {
+			return notFound
+		}
+		if bi.Main.Version == "" {
+			return notFound
+		}
+		BuildVersion = bi.Main.Version
+	}
+	return BuildVersion
+}
+
+func Time() string {
+	return time(debug.ReadBuildInfo)
+}
+
+func time(reader buildInfoReader) string {
+	bi, ok := reader()
+	if !ok {
+		return notFound
+	}
+	return tryFindSetting(vcsTime, bi.Settings...)
+}
+
+func Hash() string {
+	return hash(debug.ReadBuildInfo)
+}
+
+func hash(reader buildInfoReader) string {
+	bi, ok := reader()
+	if !ok {
+		return notFound
+	}
+	return tryFindSetting(vcsRevision, bi.Settings...)
+}
+
+func tryFindSetting(key string, settings ...debug.BuildSetting) string {
+	for _, setting := range settings {
+		if setting.Key == key {
+			return setting.Value
+		}
+	}
+	return notFound
+}

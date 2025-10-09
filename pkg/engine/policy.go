efb8da991843e6bcda94a506beb4d108bd447c29
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2025-10-09 16:35:17 +0200

    refactor: remove the need for a policy func
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/engine/policy.go
+++ pkg/engine/policy.go
@@ -5,8 +5,6 @@ import (
 	"k8s.io/client-go/dynamic"
 )
 
-type PolicyFunc func() (*authv3.CheckResponse, error)
-
 type CompiledPolicy interface {
-	For(*authv3.CheckRequest, dynamic.Interface) PolicyFunc
+	Evaluate(*authv3.CheckRequest, dynamic.Interface) (*authv3.CheckResponse, error)
 }

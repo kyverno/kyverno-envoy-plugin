8f57dedfbfb5ed942b90f2ad512b28350e4918c7
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2025-10-17 15:22:30 +0200

    refactor: move compiler package (#598)
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/engine/compiler/compiler_test.go
+++ pkg/engine/compiler/compiler_test.go
@@ -6,7 +6,7 @@ import (
 
 	corev3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
 	authv3 "github.com/envoyproxy/go-control-plane/envoy/service/auth/v3"
-	"github.com/kyverno/kyverno-envoy-plugin/pkg/engine/vpol/compiler"
+	"github.com/kyverno/kyverno-envoy-plugin/pkg/engine/compiler"
 	vpol "github.com/kyverno/kyverno/api/policies.kyverno.io/v1alpha1"
 	"github.com/stretchr/testify/assert"
 	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"

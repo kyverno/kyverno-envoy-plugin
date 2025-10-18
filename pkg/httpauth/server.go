8508ae4b457debf07328740b7b576370967084f8
aerosouund <aerosound161@gmail.com>
2025-10-18 17:35:42 +0300

    remove authorizer init
    Signed-off-by: aerosouund <aerosound161@gmail.com>

--- pkg/httpauth/server.go
+++ pkg/httpauth/server.go
@@ -4,14 +4,18 @@ import (
 	"context"
 	"net/http"
 
+	"github.com/kyverno/kyverno-envoy-plugin/pkg/engine"
 	"github.com/kyverno/kyverno-envoy-plugin/pkg/server"
+	"github.com/sirupsen/logrus"
+	"k8s.io/client-go/dynamic"
 )
 
-func NewServer(addr string, a *authorizer) server.ServerFunc {
+func NewServer(addr string, dyn dynamic.Interface, p engine.HTTPSource, nestedRequest bool, logger *logrus.Logger) server.ServerFunc {
 	return func(ctx context.Context) error {
 		// create mux
 		mux := http.NewServeMux()
 		// register health check
+		a := NewAuthorizer(dyn, p, nestedRequest, logger)
 		mux.Handle("POST /", http.HandlerFunc(a.NewHandler()))
 		// create server
 		s := &http.Server{

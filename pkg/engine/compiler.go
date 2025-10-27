841d2ffbedb18bf842c13382107794c7f4a588bf
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2025-10-27 17:17:29 +0100

    refactor: simplify source type params
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/engine/compiler.go
+++ pkg/engine/compiler.go
@@ -1,11 +1,10 @@
 package engine
 
 import (
-	"github.com/kyverno/kyverno-envoy-plugin/sdk/extensions/policy"
 	vpol "github.com/kyverno/kyverno/api/policies.kyverno.io/v1alpha1"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 )
 
-type Compiler[DATA, IN, OUT any] interface {
-	Compile(*vpol.ValidatingPolicy) (policy.Policy[DATA, IN, OUT], field.ErrorList)
+type Compiler[POLICY any] interface {
+	Compile(*vpol.ValidatingPolicy) (POLICY, field.ErrorList)
 }

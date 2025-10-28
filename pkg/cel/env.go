c0a91c074b5a6d1e43b21f692b255c85dcf85949
aerosouund <aerosound161@gmail.com>
2025-10-28 14:43:50 +0300

    fix env http ordering
    Signed-off-by: aerosouund <aerosound161@gmail.com>

--- pkg/cel/env.go
+++ pkg/cel/env.go
@@ -57,10 +57,10 @@ func NewEnv(evalMode vpol.EvaluationMode) (*cel.Env, error) {
 		library.Quantity(),
 		library.SemverLib(),
 		// register our libs
+		http.Lib(),
 		coreEnv,
 		jwt.Lib(),
 		jsoncel.Lib(&jsonimpl.JsonImpl{}),
-		http.Lib(),
 		resource.Lib(),
 		image.Lib(),
 		imagedata.Lib(),

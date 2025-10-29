6eb012490f1d567fa7034b78a4e6f690ad42e4a3
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2025-10-29 17:56:48 +0100

    refactor: git external policy source api
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/commands/run/reconciler.go
+++ pkg/commands/run/reconciler.go
@@ -7,11 +7,12 @@ import (
 	"sync"
 
 	authv3 "github.com/envoyproxy/go-control-plane/envoy/service/auth/v3"
-	"github.com/hairyhenderson/go-fsimpl"
 	"github.com/hairyhenderson/go-fsimpl/filefs"
 	"github.com/hairyhenderson/go-fsimpl/gitfs"
 	"github.com/kyverno/kyverno-envoy-plugin/apis/v1alpha1"
 	"github.com/kyverno/kyverno-envoy-plugin/pkg/authz/envoy"
+	"github.com/kyverno/kyverno-envoy-plugin/pkg/authz/http"
+	httplib "github.com/kyverno/kyverno-envoy-plugin/pkg/cel/libs/http"
 	"github.com/kyverno/kyverno-envoy-plugin/pkg/engine"
 	vpolcompiler "github.com/kyverno/kyverno-envoy-plugin/pkg/engine/compiler"
 	"github.com/kyverno/kyverno-envoy-plugin/pkg/engine/sources"
@@ -34,7 +35,7 @@ import (
 
 var (
 	envoyCompiler = vpolcompiler.NewCompiler[dynamic.Interface, *authv3.CheckRequest, *authv3.CheckResponse]()
-	// httpCompiler  = vpolcompiler.NewCompiler[dynamic.Interface, *httplib.Request, *httplib.Response]()
+	httpCompiler  = vpolcompiler.NewCompiler[dynamic.Interface, *httplib.Req, *httplib.Resp]()
 )
 
 type entry struct {
@@ -62,6 +63,17 @@ func (r *reconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Resu
 }
 
 func (r *reconciler) runServer(req ctrl.Request, object v1alpha1.AuthorizationServer) error {
+	var envoyErr, httpErr error
+	if object.Spec.Type.Envoy != nil {
+		envoyErr = r.runEnvoyServer(req, object)
+	}
+	if object.Spec.Type.Envoy != nil {
+		httpErr = r.runHttpServer(req, object)
+	}
+	return multierr.Combine(envoyErr, httpErr)
+}
+
+func (r *reconciler) runEnvoyServer(req ctrl.Request, object v1alpha1.AuthorizationServer) error {
 	r.lock.Lock()
 	defer r.lock.Unlock()
 	server := r.servers[req]
@@ -136,6 +148,81 @@ func (r *reconciler) runServer(req ctrl.Request, object v1alpha1.AuthorizationSe
 	return nil
 }
 
+func (r *reconciler) runHttpServer(req ctrl.Request, object v1alpha1.AuthorizationServer) error {
+	r.lock.Lock()
+	defer r.lock.Unlock()
+	server := r.servers[req]
+	if server == nil {
+		// create server
+		kubeConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+			clientcmd.NewDefaultClientConfigLoadingRules(),
+			nil,
+		)
+		config, err := kubeConfig.ClientConfig()
+		if err != nil {
+			return err
+		}
+		dynclient, err := dynamic.NewForConfig(config)
+		if err != nil {
+			return err
+		}
+		// create a controller manager
+		scheme := runtime.NewScheme()
+		if err := vpol.Install(scheme); err != nil {
+			return err
+		}
+		mgr, err := ctrl.NewManager(config, ctrl.Options{
+			Scheme: scheme,
+			Metrics: metricsserver.Options{
+				BindAddress: "0",
+			},
+		})
+		if err != nil {
+			return fmt.Errorf("failed to construct manager: %w", err)
+		}
+		// create a cancellable context
+		ctx := context.Background()
+		ctx, cancel := context.WithCancel(ctx)
+		// create a wait group
+		var group wait.Group
+		// track errors
+		var grpcErr, mgrErr error
+		group.StartWithContext(ctx, func(ctx context.Context) {
+			// cancel context at the end
+			defer cancel()
+			mgrErr = mgr.Start(ctx)
+		})
+		if !mgr.GetCache().WaitForCacheSync(ctx) {
+			defer cancel()
+			return fmt.Errorf("failed to wait for cache sync")
+		}
+		src, err := buildSources(mgr, httpCompiler, object)
+		if err != nil {
+			return fmt.Errorf("failed to build engine source: %w", err)
+		}
+		http := http.NewServer(object.Spec.Type.HTTP.Address, dynclient, src, false)
+		group.StartWithContext(ctx, func(ctx context.Context) {
+			// grpc auth server
+			defer cancel()
+			grpcErr = http.Run(ctx)
+		})
+		server = &entry{
+			cancel: func() error {
+				// cancel context
+				cancel()
+				// wait all tasks in the group are over
+				group.Wait()
+				return multierr.Combine(grpcErr, mgrErr)
+			},
+		}
+		r.servers[req] = server
+		return nil
+	}
+	// configure server
+	// TODO
+	return nil
+}
+
 func (r *reconciler) stopServer(req ctrl.Request) error {
 	r.lock.Lock()
 	defer r.lock.Unlock()
@@ -149,8 +236,6 @@ func (r *reconciler) stopServer(req ctrl.Request) error {
 
 func buildSources[POLICY any](mgr ctrl.Manager, compiler engine.Compiler[POLICY], server v1alpha1.AuthorizationServer) (core.Source[POLICY], error) {
 	var out []core.Source[POLICY]
-	mux := fsimpl.NewMux()
-	mux.Add(gitfs.FS)
 	for _, src := range server.Spec.Sources {
 		if src.Kubernetes != nil {
 			// TODO: selector
@@ -160,8 +245,12 @@ func buildSources[POLICY any](mgr ctrl.Manager, compiler engine.Compiler[POLICY]
 			}
 			out = append(out, source)
 		}
-		if src.External != nil {
-			fsys, err := mux.Lookup(src.External.URL)
+		if src.Git != nil {
+			u, err := url.Parse("file://" + src.Fs.Path)
+			if err != nil {
+				return nil, err
+			}
+			fsys, err := gitfs.New(u)
 			if err != nil {
 				return nil, err
 			}

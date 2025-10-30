19aab554eb6d29f758abb5562fd9907aa8d48a72
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2025-10-30 13:07:45 +0100

    fix server
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/authz/http/server.go
+++ pkg/authz/http/server.go
@@ -19,7 +19,7 @@ func NewServer(addr string, dyn dynamic.Interface, p engine.HTTPSource, nestedRe
 			dyn:           dyn,
 			nestedRequest: nestedRequest,
 		}
-		mux.Handle("POST /", a.NewHandler())
+		mux.Handle("POST /", a)
 		// create server
 		s := &http.Server{
 			Addr:    addr,

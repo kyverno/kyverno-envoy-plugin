84f85e8d6219563652f11d89e2805d64386082d4
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2025-11-06 09:41:12 +0100

    feat: add cache to kube source
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/engine/sources/kube.go
+++ pkg/engine/sources/kube.go
@@ -1,6 +1,8 @@
 package sources
 
 import (
+	"context"
+
 	"github.com/kyverno/kyverno-envoy-plugin/pkg/engine"
 	"github.com/kyverno/kyverno-envoy-plugin/sdk/core"
 	"github.com/kyverno/kyverno-envoy-plugin/sdk/core/sources"
@@ -20,10 +22,15 @@ func NewKube[POLICY any](name string, mgr ctrl.Manager, compiler engine.Compiler
 		return nil, err
 
 	}
-	transform := sources.NewTransformErr(apis, func(in *v1alpha1.ValidatingPolicy) (POLICY, error) {
-		policy, err := compiler.Compile(in)
-		return policy, err.ToAggregate()
-	})
-	// TODO: cache
-	return transform, nil
+	cache := sources.NewCache(
+		apis,
+		func(_ context.Context, in *v1alpha1.ValidatingPolicy) (string, error) {
+			return in.Name + in.ResourceVersion, nil
+		},
+		func(_ context.Context, _ string, in *v1alpha1.ValidatingPolicy) (POLICY, error) {
+			policy, err := compiler.Compile(in)
+			return policy, err.ToAggregate()
+		},
+	)
+	return cache, nil
 }
